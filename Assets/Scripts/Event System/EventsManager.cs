9cfc936c2fa0e7129bdf158afc26712b357cea98
Mingxuan Li <mingxual@usc.edu>
2020-09-08 21:45:16 -0700

    iTween and LeanTween tools added with a copy of theatre_collision 1 for test purpose

---
+++ Assets/Scripts/Event System/EventsManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsManager : MonoBehaviour
+{
+    // Singleton class
+    public static EventsManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+
+}
