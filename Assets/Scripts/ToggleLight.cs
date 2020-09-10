827df013de9f98475c97b62fea8030d5d4f3ec7a
Mingxuan Li <mingxual@usc.edu>
2020-09-09 22:48:08 -0700

    Edit the ceiling spotlights in theatre v2

---
+++ Assets/Scripts/ToggleLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLight : MonoBehaviour
+{
+    public KeyCode toggleKey;
+    private Light m_Light;
+    private bool isChanged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Light = GetComponent<Light>();
+        isChanged = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(toggleKey) && !isChanged)
+        {
+            m_Light.enabled = !m_Light.enabled;
+            isChanged = true;
+        }
+        else if(Input.GetKeyUp(toggleKey))
+        {
+            isChanged = false;
+        }
+    }
+}
