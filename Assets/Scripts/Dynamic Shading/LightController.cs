f5524f9008ad4b0f46b75766e6709326f24388ab
1010meha <44281943+1010meha@users.noreply.github.com>
2020-09-22 16:41:38 -0700

    Added Theatre v3.2-meha (has some animation, sound, and post processing)
    Tested out some stuff- altered the spotlight move code a bit so I could make the animation work

--- Assets/Scripts/Dynamic Shading/LightController.cs
+++ Assets/Scripts/Dynamic Shading/LightController.cs
@@ -7,25 +7,60 @@ public class LightController : MonoBehaviour
     public float moveSpeed;
     public float rotateSpeed;
 
+    public Animator anim;
+    public GameObject luxModel;
+    public GameObject EmptyGO;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            anim.SetBool("Moving", true);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            anim.SetBool("Moving", true);
+
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+            anim.SetBool("Moving", true);
+
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+            anim.SetBool("Moving", true);
+
         }
+
+        //he stops running animation when no key is pressed
+        if (Input.anyKey == false)
+        {
+            anim.SetBool("Moving", false);
+        }
+
+        //I just hardcoded lux's size for testing purposes
+        /* if (Input.GetKeyDown(KeyCode.LeftArrow))
+         {
+             luxModel.transform.localScale = new Vector3(14.3759f, 14.3759f, -14.3759f);
+         }
+         if (Input.GetKeyDown(KeyCode.RightArrow))
+         {
+             luxModel.transform.localScale = new Vector3(14.3759f, 14.3759f, 14.3759f);
+         }*/
+
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 newPosition = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        luxModel.transform.LookAt(newPosition + luxModel.transform.position);
+        //luxModel.transform.Translate(newPosition * Time.deltaTime, Space.World);
+
         /*if (Input.GetKey(KeyCode.L))
         {
             transform.Rotate(0.0f, rotateSpeed * Time.deltaTime, 0.0f);
---
+++ Assets/TestAudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAudioManager : MonoBehaviour
+{
+    public AudioSource randomSound;
+
+    public AudioClip[] audioSources;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayLuxFootstep()
+    {
+        Debug.Log("PrintEvent: " + " called at: " + Time.time);
+        randomSound.clip = audioSources[Random.Range(0, audioSources.Length)];
+        randomSound.Play();
+    }
+}
