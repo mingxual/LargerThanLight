2f89a2df7e95c2e7cd8ef4aaa9de1af4ad5608a0
Mingxuan Li <mingxual@usc.edu>
2020-09-22 18:49:38 -0700

    Menu Scene Created

---
+++ Assets/Scripts/Outside/MenuManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager instance;
+
+    public GameObject mainPage;
+    public GameObject optionPage;
+    public GameObject creditPage;
+    public GameObject quitPrompt;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPage.SetActive(true);
+        optionPage.SetActive(false);
+        creditPage.SetActive(false);
+        quitPrompt.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region MainPage
+    public void OnClickStartBtn()
+    {
+
+    }
+
+    public void OnClickOptionBtn()
+    {
+        optionPage.SetActive(true);
+    }
+
+    public void OnClickCreditBtn()
+    {
+        creditPage.SetActive(true);
+    }
+
+    public void OnClickQuitBtn()
+    {
+        quitPrompt.SetActive(true);
+    }
+
+    #endregion
+
+    #region OptionPage
+    public void OnClickBackBtn()
+    {
+        optionPage.SetActive(false);
+    }
+
+    #endregion
+
+    #region CreditPage
+    
+    #endregion
+
+    #region QuitPrompt
+
+    public void OnClickConfirmBtn()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickCancelBtn()
+    {
+        quitPrompt.SetActive(false);
+    }
+    #endregion
+}
