b0e3d89a067f01bb14270e59134841b125b2fc3b
Richard Ortega-Amezcua <ortegaam@usc.edu>
2020-10-03 22:11:44 -0700

    Enabling character control
    idk wtf im doing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,18 +7,20 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     //Obstacle and its mesh/vertices
-    public static GameObject[] allObstacles;
-    public static Mesh[] allMeshes;
+    public static List<GameObject> allObstacles;
+    public static List<Mesh> allMeshes;
     public static List<List<Vector3>> meshVertices;
 
     //Pool of N game objects
-    public int poolCount = 10;
+    //public int poolCount = 10;
     public static List<GameObject> gameObjectPool;
     public static List<EdgeCollider2D> edgeCollider2DPool; //Keep a reference to pooled game object's edge collider
     public static bool hasResettedColliderPool = false;
 
     GameObject gameObjectParent;
-    public GameObject originWall;
+    public GameObject[] m_SubLevel1Obstacles;
+    public GameObject[] m_SubLevel2Obstacles;
+    public GameObject originWallSubLevel1;
     public GameObject m_SubLevel1Object;
     public Wall3D m_SubLevel1Wall3D;
     public GameObject m_SubLevel2Object;
@@ -31,19 +33,24 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         //Get all shadow-casting obstacles in scene by their tag "Obstacle" as well their mesh
-        allObstacles = GameObject.FindGameObjectsWithTag("Obstacle");
-        allMeshes = new Mesh[allObstacles.Length];
+        allObstacles = new List<GameObject>();
+        allMeshes = new List<Mesh>();
+
+        for(int i = 0; i < m_SubLevel1Obstacles.Length; i++)
+        {
+            allObstacles.Add(m_SubLevel1Obstacles[i]);
+        }
 
         //Store mesh vertices
         meshVertices = new List<List<Vector3>>();
-        for (int i = 0; i < allMeshes.Length; ++i)
+        for (int i = 0; i < m_SubLevel1Obstacles.Length; ++i)
         {
-            allMeshes[i] = allObstacles[i].GetComponent<MeshFilter>().sharedMesh;
+            allMeshes.Add(m_SubLevel1Obstacles[i].GetComponent<MeshFilter>().mesh);
             int numVertices = allMeshes[i].vertexCount;
             List<Vector3> vertexPositions = new List<Vector3>();
             for (int j = 0; j < numVertices; j++)
             {
-                Vector3 pos = allObstacles[i].transform.localToWorldMatrix * allMeshes[i].vertices[j];
+                Vector3 pos = m_SubLevel1Obstacles[i].transform.localToWorldMatrix * allMeshes[i].vertices[j];
                 if (!vertexPositions.Contains(pos))
                     vertexPositions.Add(pos);
             }
@@ -64,7 +71,7 @@ public class GameManager : MonoBehaviour
         physicsMaterial.bounciness = 0.0f;
 
         //Create pool of gameobjects and store edge colliders
-        for (int i = 0; i < poolCount; ++i)
+        for (int i = 0; i < m_SubLevel1Obstacles.Length; ++i)
         {
             GameObject gameObject = new GameObject();
             gameObject.layer = 10;
@@ -119,6 +126,49 @@ public class GameManager : MonoBehaviour
         m_SubLevel1Object.SetActive(false);
         m_SubLevel2Object.SetActive(true);
         skiaControl.gameObject.transform.position = SkiaSpawnSubLevel2.position;
+        AddNewObstacles(m_SubLevel2Obstacles);
         Debug.Log("moving Skia");
+    }
+
+    void AddNewObstacles(GameObject[] gameobjectsToAdd)
+    {
+        for (int i = 0; i < gameobjectsToAdd.Length; i++)
+        {
+            allObstacles.Add(gameobjectsToAdd[i]);
+        }
+
+        for (int i = 0; i < gameobjectsToAdd.Length; ++i)
+        {
+            allMeshes.Add(gameobjectsToAdd[i].GetComponent<MeshFilter>().mesh);
+            int numVertices = allMeshes[i].vertexCount;
+            List<Vector3> vertexPositions = new List<Vector3>();
+            for (int j = 0; j < numVertices; j++)
+            {
+                Vector3 pos = gameobjectsToAdd[i].transform.localToWorldMatrix * allMeshes[i].vertices[j];
+                if (!vertexPositions.Contains(pos))
+                    vertexPositions.Add(pos);
+            }
+            //Debug.Log("Adding " + vertexPositions.Count + " vertices");
+            meshVertices.Add(vertexPositions);
+        }
+
+        PhysicsMaterial2D physicsMaterial = new PhysicsMaterial2D();
+        physicsMaterial.name = "Edge collider physics material";
+        physicsMaterial.friction = 0.0f;
+        physicsMaterial.bounciness = 0.0f;
+
+        //Create pool of gameobjects and store edge colliders
+        for (int i = 0; i < gameobjectsToAdd.Length; ++i)
+        {
+            GameObject gameObject = new GameObject();
+            gameObject.layer = 10;
+            gameObject.transform.SetParent(gameObjectParent.transform);
+            gameObject.transform.position = gameObjectParent.transform.position;
+            EdgeCollider2D edgeCollider2D = gameObject.AddComponent<EdgeCollider2D>();
+            edgeCollider2D.sharedMaterial = physicsMaterial;
+            edgeCollider2DPool.Add(edgeCollider2D);
+            gameObject.SetActive(false);
+            gameObjectPool.Add(gameObject);
+        }
     }
 }
