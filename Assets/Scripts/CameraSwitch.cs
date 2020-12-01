6e9d53fcdec22656258f26ab42260a4ae4f25148
Mingxuan Li <mingxual@usc.edu>
2020-12-01 12:11:17 +0800

    Modify fadeout effect

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -8,6 +8,7 @@ public class CameraSwitch : MonoBehaviour
     public GameObject originCamera;
     public float transitionTime;
     public List<GameObject> cameraWalls;
+    public float duration = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -38,9 +39,34 @@ public class CameraSwitch : MonoBehaviour
     {
         foreach (GameObject go in cameraWalls)
         {
+            StartCoroutine(FadeTo(go.GetComponent<MeshRenderer>().material, 0f, duration));
+            // Color temp = go.GetComponent<MeshRenderer>().material.color;
+            // temp.a = 0;
+            // go.GetComponent<MeshRenderer>().material.color = temp;
             // I cannot get the walls to fade out properly
             // the banners go invisible in front of the material
-            go.SetActive(false);
+            // go.SetActive(false);
+        }
+    }
+
+    IEnumerator FadeTo(Material material, float targetOpacity, float duration)
+    {
+        Color color = material.color;
+        float startOpacity = color.a;
+
+        float t = 0;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+
+            float blend = Mathf.Clamp01(t / duration);
+
+            color.a = Mathf.Lerp(startOpacity, targetOpacity, blend);
+
+            material.color = color;
+
+            yield return new WaitForEndOfFrame();
         }
     }
 }
