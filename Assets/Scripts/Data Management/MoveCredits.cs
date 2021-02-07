e39103487d80c1d57b62600abd28597ac6fde161
Yining Zhang <yzhang39@usc.edu>
2021-02-07 23:09:34 +0800

    Credit Scene Added

---
+++ Assets/Scripts/Data Management/MoveCredits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCredits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localPosition = new Vector3(0, -100, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localPosition += new Vector3(0, 150 * Time.deltaTime, 0);
+    }
+}
