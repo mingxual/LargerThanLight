bf9035f05550844ff1aa4f7fc11520cd600fe844
1010meha <44281943+1010meha@users.noreply.github.com>
2021-02-17 13:31:24 -0800

    narrative stuff, intro 'scene'
    also slightly moved one obstacle in the hallway to make it playable- hoping that it didnt break anything

---
+++ Assets/StartCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCam : MonoBehaviour
+{
+    public Camera gameCam;
+    public GameObject startCamera;
+    public SkinnedMeshRenderer skiaMeshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void CameraSwitch()
+    {
+        gameCam.enabled = true;
+        skiaMeshRenderer.enabled = true;
+        Destroy(startCamera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
