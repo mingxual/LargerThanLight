548663815898b863f78efb98dfc26b6e486ab081
Yining Zhang <yzhang39@usc.edu>
2021-02-18 19:52:18 +0800

    Build Scenes Uploaded

---
+++ Assets/Scripts/Transitions/Transition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    [SerializeField] GameObject Lux;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name.Equals(Lux.name))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
