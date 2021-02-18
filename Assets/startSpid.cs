8bb71cd83f0cf2dd7c3bd55fbeb0285c11ff4fa9
1010meha <44281943+1010meha@users.noreply.github.com>
2021-02-17 19:46:33 -0800

    added transitional scene

---
+++ Assets/startSpid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startSpid : MonoBehaviour
+{
+    public GameObject spiderSprite;
+    public GameObject spiderSculpture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.gameObject.transform.position.x <= -212f)
+        {
+            spiderSprite.SetActive(true);
+            spiderSculpture.SetActive(false);
+
+        }
+    }
+
+}
