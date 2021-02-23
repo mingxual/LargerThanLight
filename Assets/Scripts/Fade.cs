7dda7fd8823aceae01765f2f16389e33f56004d1
Richard Ortega-Amezcua <ortegaam@usc.edu>
2021-02-22 17:54:36 -0800

    scuffed fade in theatre

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    private Material m_Material;
+    [SerializeField] float m_TotalFadeTime = 2.0f;
+    float m_CurrFadeTime = 0.0f;
+    private bool m_IsFading = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Material = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_IsFading)
+        {
+            m_CurrFadeTime += Time.deltaTime;
+            Color color = m_Material.color;
+            color.a = Mathf.Lerp(1.0f, 0.0f, (m_CurrFadeTime / m_TotalFadeTime));
+            m_Material.color = color;
+        }
+    }
+
+    public void FadeObject()
+    {
+        if (!m_Material) m_Material = GetComponent<MeshRenderer>().material;
+        m_IsFading = true;
+    }
+}
