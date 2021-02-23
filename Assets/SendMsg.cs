f4fcaa23e9edecd3992345ebe71f5a908f37a86f
1010meha <44281943+1010meha@users.noreply.github.com>
2021-02-23 01:43:57 -0800

    gym dialogue start

---
+++ Assets/SendMsg.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class SendMsg : MonoBehaviour
+{
+    public Flowchart mflowchart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void WireOne()
+    {
+        mflowchart.SendFungusMessage("one");
+    }
+    public void WireTwo()
+    {
+        mflowchart.SendFungusMessage("two");
+    }
+    public void WireThree()
+    {
+        mflowchart.SendFungusMessage("three");
+    }
+}
