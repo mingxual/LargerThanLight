400b24c2d2b61cf80e56fe096caadfe39fa70fbe
VenusMeow <venusyixinsun@gmail.com>
2021-02-24 15:11:37 +0800

    ui quick fixes

---
+++ Assets/Scripts/UI/UITimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITimer : MonoBehaviour
+{
+    public float time = 5f;
+    public bool keyPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Hide", time);
+    }
+
+    private void Update()
+    {
+        if (keyPressed)
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.UpArrow) ||Input.GetKey(KeyCode.DownArrow))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
