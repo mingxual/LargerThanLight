e3dcc39668b0a1a0c5d33f307c9b3e1e8bb5574e
1010meha <44281943+1010meha@users.noreply.github.com>
2021-02-24 16:22:25 -0800

    tweeks x2

--- Assets/Scripts/Data Management/MoveCredits.cs
+++ Assets/Scripts/Data Management/MoveCredits.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MoveCredits : MonoBehaviour
 {
+    public float endHere;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,10 @@ public class MoveCredits : MonoBehaviour
     void Update()
     {
         this.transform.localPosition += new Vector3(0, 150 * Time.deltaTime, 0);
+        if (this.transform.position.y >= endHere)
+        {
+            SceneManager.LoadScene("1STARTHERE");
+        }
+
     }
 }
