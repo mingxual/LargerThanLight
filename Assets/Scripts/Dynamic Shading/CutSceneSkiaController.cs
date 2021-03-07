086dfa77d327714df4102146b1e29a17cd8cf311
fourdimension <37730913+fourdimension@users.noreply.github.com>
2021-03-07 14:09:07 -0800

    Add controller in cutsscene to control Skia's movement

---
+++ Assets/Scripts/Dynamic Shading/CutSceneSkiaController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneSkiaController : MonoBehaviour
+{
+
+    [SerializeField] private Animator anim;
+    [SerializeField] private Transform skiaModel;
+    [SerializeField] Vector3 forwardDir;
+    public bool isRunning;
+    private Vector3 movementDir;
+    private Vector3 rightDir;
+
+    private void Awake()
+    {
+        movementDir = Vector3.zero;
+        rightDir = Vector3.Cross(Vector3.up, forwardDir);
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        isRunning = false;
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            isRunning = true;
+            skiaModel.LookAt(skiaModel.position - rightDir);
+            anim.SetBool("IsRunning", isRunning);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            isRunning = true;
+            skiaModel.LookAt(skiaModel.position + rightDir);
+            anim.SetBool("IsRunning", isRunning);
+        }
+        else {
+            anim.SetBool("IsRunning", false);
+        }
+    }
+    private void SetForwardDir(Vector3 dir) { 
+        
+    }
+}
