10044aa1850c854c0361311fb31f05cf113e222f
my-lu <mlu98021@usc.edu>
2021-03-23 19:51:00 -0700

    fixed the credit scene

--- Assets/Scripts/Data Management/MoveCredits.cs
+++ Assets/Scripts/Data Management/MoveCredits.cs
@@ -6,21 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class MoveCredits : MonoBehaviour
 {
-    public float endHere;
-    // Start is called before the first frame update
-    void Start()
+    public void EndScene()
     {
-        this.transform.localPosition = new Vector3(0, -600, 0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        this.transform.localPosition += new Vector3(0, 15, 0);
-        if (this.transform.position.y >= endHere)
-        {
-            SceneManager.LoadScene("1STARTHERE");
-        }
-
+        SceneManager.LoadScene("1STARTHERE");
     }
 }
