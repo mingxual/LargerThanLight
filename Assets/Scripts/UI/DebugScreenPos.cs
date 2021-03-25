6454c3e85393774bdfd1acdc1b1fb8389387b2e9
Mingxuan Li <mingxual@usc.edu>
2021-03-26 00:53:20 +0800

    Add UIController which enables more customization in displaying UI

---
+++ Assets/Scripts/UI/DebugScreenPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScreenPos : MonoBehaviour
+{
+    public SimpleController skia;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 screenPos = cam.WorldToScreenPoint(skia.GetWorldPosition3D());
+        Debug.Log("Skia's screen position is " + screenPos.x + " " + screenPos.y + " " + screenPos.z);
+    }
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Camera cam;
+    public SimpleController skia;
+
+    // Image frame that would hold the images
+    public Image frame;
+    // An array of UI pictures to show
+    public List<Sprite> images;
+    // Offset to the skia screen position
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Test
+        /*
+        Vector3 framePos = GetScreenPos() + offset;
+        framePos.z = 0;
+        frame.transform.position = framePos;
+        */
+    }
+
+    // Return the skia's screen position relative to the cam
+    public Vector3 GetScreenPos()
+    {
+        return cam.WorldToScreenPoint(skia.GetWorldPosition3D());
+    }
+
+    public void DisplayUI(int index)
+    {
+        if (index < 0 || index >= images.Count)
+            Debug.LogError("The index passed is wrong");
+
+        frame.sprite = images[index];
+        frame.gameObject.SetActive(true);
+        
+        Vector3 framePos = GetScreenPos() + offset;
+        framePos.z = 0;
+        frame.transform.position = framePos;
+    }
+
+    public void CloseUI()
+    {
+        frame.gameObject.SetActive(false);
+    }
+}
