4b08ae93f93f80ccbe3d703ba207fb7f2dd546be
1010meha <44281943+1010meha@users.noreply.github.com>
2021-03-28 16:48:17 -0700

    Revert "Disabled Skia's controls during respawning"
    This reverts commit 4f205b70a63572edc462047338bd686256f79c7f.

--- Assets/Scripts/Dynamic Shading/SimpleController.cs
+++ Assets/Scripts/Dynamic Shading/SimpleController.cs
@@ -325,12 +325,10 @@ public class SimpleController : MonoBehaviour
         m_IsDead = true; // Set dead status
         Destroy(Instantiate(m_DeathParticleEffect, oldPosition, transform.rotation), 1.0f); // Instantiate the instant death effect
         GameObject dragPart = Instantiate(m_DraggingParticleEffect, oldPosition, transform.rotation); // Instantiate the dragging particle effect that will move towards spawn
-        MoveToOrigin moveToOrigin = dragPart.GetComponent<MoveToOrigin>();
-        moveToOrigin.target = m_SpawnPosition2D; // Set dragging particle effect's target location to move toward
-        moveToOrigin.thePlayerMesh = m_SkinnedMeshRenderer; // Set Skia's mesh renderer to drag particle so it can turn on Skia's mesh once it reaches location
-        moveToOrigin.m_SimpleController = this;
-        enabled = false; // Disable controls
+        dragPart.GetComponent<MoveToOrigin>().target = m_SpawnPosition2D; // Set dragging particle effect's target location to move toward
+        dragPart.GetComponent<MoveToOrigin>().thePlayerMesh = m_SkinnedMeshRenderer; // Set Skia's mesh renderer to drag particle so it can turn on Skia's mesh once it reaches location
     }
+
 
     /// <summary>
     /// Resets Skia to current spawnpoint
