148be0ffc10abf4a5ffa5abb0857e4bc9eb1a215
Mingxuan Li <mingxual@usc.edu>
2021-04-03 20:32:11 +0800

    Change UI menu stuff

---
+++ Assets/Scripts/UI/DarkMaskController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DarkMaskController : MonoBehaviour
+{
+    public Slider slide;
+    public Image frame;
+    private float ratio;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slide.value = slide.maxValue;
+        ratio = 1.0f;
+        AdjustToRightAmount();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeBrightness()
+    {
+        ratio = slide.value;
+        AdjustToRightAmount();
+    }
+
+    private void AdjustToRightAmount()
+    {
+        Color currColor = frame.color;
+        currColor.a = 1.0f - ratio;
+        frame.color = currColor;
+    }
+}
---
+++ Assets/Scripts/UI/DontDestroy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour
+{
+    public GameObject controlRoot;
+    public bool isFirstScene = true;
+
+    [SerializeField] List<GameObject> subChildren;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isFirstScene)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (controlRoot.activeInHierarchy == false)
+                {
+                    controlRoot.SetActive(true);
+                    // Time.timeScale = 0;
+                }
+                else
+                {
+                    controlRoot.SetActive(false);
+                    // Time.timeScale = 1;
+                }
+            }
+        }
+    }
+
+    public void SwitchToNotFirstScene()
+    {
+        isFirstScene = false;
+
+        for (int i = 0; i < subChildren.Count; i++)
+            subChildren[i].SetActive(true);
+
+        controlRoot.SetActive(false);
+    }
+
+    /*
+    public void QuiteApp()
+    {
+        Application.Quit();
+    }
+    public void CloseOptions()
+    {
+        controlRoot.SetActive(false);
+        // Time.timeScale = 1;
+    }
+    */
+}
