9ea85ab748360952d51b9330f88b019f59f552e0
1010meha <44281943+1010meha@users.noreply.github.com>
2021-04-06 17:50:59 -0700

    i tried a thing

---
+++ Assets/startDer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startDer : MonoBehaviour
+{
+    public GameObject lux;
+    public float pos = -74;
+    public GameObject derAnim;
+    public GameObject sculptures;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lux.transform.position.x <= pos)
+        {
+            derAnim.SetActive(true);
+            sculptures.SetActive(false);
+        }
+    }
+}
