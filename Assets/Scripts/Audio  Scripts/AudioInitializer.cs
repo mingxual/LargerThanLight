7b9f8e5e64495557b7c2ac10729f7e98bad99636
JustinGu <runshigu@usc.edu>
2021-04-12 11:58:57 +0800

    quick fix

---
+++ Assets/Scripts/Audio  Scripts/AudioInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioInitializer : MonoBehaviour
+{
+    public void InitializeLocker()
+    {
+        AudioManager.instance.InitializeLockerLevel();
+    }
+    public void InitializeTheater()
+    {
+        AudioManager.instance.InitializeTheaterLevel();
+    }
+    public void InitializeGym()
+    {
+        AudioManager.instance.InitializeGymLevel();
+    }
+}
