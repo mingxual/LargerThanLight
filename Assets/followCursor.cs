2c3769556958a7c77f2d75dd7f50859aae0ed425
1010meha <44281943+1010meha@users.noreply.github.com>
2021-04-13 14:15:30 -0700

    fixed stuff

--- Assets/followCursor.cs
+++ Assets/followCursor.cs
@@ -11,32 +11,8 @@ public class followCursor : MonoBehaviour
 
     void Update()
     {      //mousePos
-           /* mousePosition = Input.mousePosition;
+           mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);*/
-
-        //lightPos
-        //keyboardPosition = this.transform.position;
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            keyboardPosition.transform.Translate(Vector3.up * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            keyboardPosition.transform.Translate(Vector3.down * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            keyboardPosition.transform.Translate(Vector3.left * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            keyboardPosition.transform.Translate(Vector3.right * Time.deltaTime);
-        }
-
-        transform.position = Vector2.Lerp(transform.position, keyboardPosition.transform.position, moveSpeed);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);       
     }
 }
