d2bc6cbb38b783021a62cd5e5512536f756de774
1010meha <44281943+1010meha@users.noreply.github.com>
2021-04-12 21:10:42 -0700

    aaa

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -4,28 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    public GameObject loadingScreen;
     public string levelName;
 
-    public void LoadLevel(string levelName)
+    public void LoadLevel()
     {
-        StartCoroutine(AsynchronousLoad(levelName));
+        SceneManager.LoadScene(levelName);
     }
-    IEnumerator AsynchronousLoad(string scene)
-    {
-        yield return null;
-        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
-        ao.allowSceneActivation = false;
-        loadingScreen.SetActive(true);
-
-        while (!ao.isDone)
-        {
-            // [0, 0.9] > [0, 1]
-            float progress = Mathf.Clamp01(ao.progress / 0.9f);
-            Debug.Log("Loading progress: " + (progress * 100) + "%");
-            // Loading completed          
-            ao.allowSceneActivation = true;
-            yield return null;
-        }
-    }
+    
 }
--- Assets/followCursor.cs
+++ Assets/followCursor.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class followCursor : MonoBehaviour
 {
     private Vector3 mousePosition;
+    public GameObject keyboardPosition;
     public float moveSpeed = 0.1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
     void Update()
-    {      
-            mousePosition = Input.mousePosition;
+    {      //mousePos
+           /* mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);        
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);*/
+
+        //lightPos
+        //keyboardPosition = this.transform.position;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            keyboardPosition.transform.Translate(Vector3.up * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            keyboardPosition.transform.Translate(Vector3.down * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            keyboardPosition.transform.Translate(Vector3.left * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            keyboardPosition.transform.Translate(Vector3.right * Time.deltaTime);
+        }
+
+        transform.position = Vector2.Lerp(transform.position, keyboardPosition.transform.position, moveSpeed);
     }
 }
