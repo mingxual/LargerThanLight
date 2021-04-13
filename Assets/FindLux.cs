11ddf9dbd38cdfd96cc996ee09da51102590e8e4
1010meha <44281943+1010meha@users.noreply.github.com>
2021-04-13 13:58:14 -0700

    lux arrow

---
+++ Assets/FindLux.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindLux : MonoBehaviour
+{
+    public GameObject lux;
+    public float rightBorder;
+    public float leftBorder;
+
+    public GameObject rightUI;
+    public GameObject leftUI;
+
+    void Update()
+    {
+        if (GetComponent<Camera>().WorldToScreenPoint(lux.transform.position).x > (Screen.width / 2) + rightBorder)
+        {
+            Debug.Log("Lux is right of the middle of the screen.");
+            rightUI.SetActive(true);
+            leftUI.SetActive(false);
+
+
+        }
+        else if (GetComponent<Camera>().WorldToScreenPoint(lux.transform.position).x < (Screen.width / 2) - leftBorder)
+        {
+            Debug.Log("Lux is left of the middle of the screen.");
+            leftUI.SetActive(true);
+            rightUI.SetActive(false);
+
+        }
+        else
+        {
+            leftUI.SetActive(false);
+            rightUI.SetActive(false);
+
+        }
+
+    }
+}
+
