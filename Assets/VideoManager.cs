3ba66c67620e114d12d146a2d51085ad0f9d58d5
1010meha <44281943+1010meha@users.noreply.github.com>
2021-04-13 23:26:24 -0700

    end cutscene
    still need Lux VO tho

---
+++ Assets/VideoManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    public List<GameObject> slideList = new List<GameObject>();
+
+    public GameObject videoClip;
+    public int ccurrentClup;
+
+    private void Start()
+    {
+        videoClip = slideList[0];
+        ccurrentClup = 0;
+    }
+
+    public void NextSlide()
+    {
+        Debug.Log("Next");
+        slideList[ccurrentClup + 2].GetComponent<VideoPlayer>().enabled = true;
+        slideList[ccurrentClup + 1].GetComponent<MeshRenderer>().enabled = true;
+        slideList[ccurrentClup].GetComponent<MeshRenderer>().enabled = false;
+        ccurrentClup++;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            
+            NextSlide();
+        }
+    }
+}
