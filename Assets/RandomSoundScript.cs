b15bee9c1fd90f8f51847b82784f3a9058914f64
1010meha <44281943+1010meha@users.noreply.github.com>
2021-04-14 14:58:14 -0700

    sfx stuff

---
+++ Assets/RandomSoundScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundScript : MonoBehaviour
+{
+
+    public AudioSource randomSound;
+
+    public AudioClip[] audioSources;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        CallAudio();
+    }
+
+
+    void CallAudio()
+    {
+        Invoke("RandomSoundness", Random.Range(1.0f, 10.0f));
+    }
+
+    void RandomSoundness()
+    {
+        randomSound.clip = audioSources[Random.Range(0, audioSources.Length)];
+        randomSound.Play();
+        CallAudio();
+    }
+}
